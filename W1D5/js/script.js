b78439ea0aa8737ed98ad97353bd134adcf27719
SherifZekry93 <prosherifzekry@gmail.com>
2020-04-25 14:46:30 -0500

    adding all

--- W1D5/js/script.js
+++ W1D5/js/script.js
@@ -1,9 +1,13 @@
 
 function onSubmitClick()
 {
+    
     let emailFieldValue = document.getElementById("exampleInputEmail1").value;
+    
     let passwordValue = document.getElementById("exampleInputPassword1").value;
+    
     let URLValue = document.getElementById("exampleInputURL").value;
+    
     console.log(
         "emailField :" + emailFieldValue +"\n"+
         "passwordvalue :" + passwordValue +"\n"+
